9541b1f29c5a6adac8bfdcbe5fe5aa6b9e234532
CarlosM <G00429041@atu.ie>
2024-10-17 19:22:22 +0100

    step1

---
+++ src/main/java/ie/atu/week4/Student.java
@@ -0,0 +1,45 @@
+package ie.atu.week4;
+
+public class Student {
+        // Private fields
+        private String name;
+        private String email;
+        private String course;
+
+        public Student(){
+
+        }
+        public Student(String name, String email, String course){
+            this.name = name;
+            this.email = email;
+            this.course = course;
+        }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
